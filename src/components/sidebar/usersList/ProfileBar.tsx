48c667b2a1a7ff2733c4b1e588ef53b01352b37f
Gauthier Bosson <bosson.gauthier27@gmail.com>
2021-02-11 10:26:13 +0100

    added name to profile bar

--- src/components/sidebar/usersList/ProfileBar.tsx
+++ src/components/sidebar/usersList/ProfileBar.tsx
@@ -4,9 +4,12 @@ import { Flex, Avatar, AvatarBadge, HStack } from '@chakra-ui/react'
 const ProfileBar = () => {
   return (
     <Flex justify="space-between" align="center" bgColor="discordGrey.300" px={3}>
-      <Avatar size="sm">
-        <AvatarBadge boxSize="1em" bg="green.500" />
-      </Avatar>
+      <HStack>
+        <Avatar size="sm">
+          <AvatarBadge boxSize="1em" bg="green.500" />
+        </Avatar>
+        <span>Name</span>
+      </HStack>
       <HStack>
         <span>ic</span>
         <span>ic</span>
